c618088d76fc9764887132721cb20362785962d0
Anton Curanz <anton@curanz.de>
2024-10-26 18:44:03 +0200

    new wallets

--- simulations/simu5g_emulation/start-agent.sh
+++ simulations/simu5g_emulation/start-agent.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <number>"
+if [ "$#" -lt 1 ]; then
+    echo "Usage: $0 <number> [OPTS]"
     exit 1
 fi
 netns=$(( $1 + 1 ))
-sudo podman run --rm -it --network "ns:/run/netns/ns$netns" ghcr.io/antoncuranz/didcomm-on-quic/agents agents.car.main "car$1s" --ip "192.168.$netns.2" --ledger http://192.168.1.2:8000
+sudo podman run --rm -it --name "car$1s" --network "ns:/run/netns/ns$netns" ghcr.io/antoncuranz/didcomm-on-quic/agents agents.car.main "car$1s" --ip "192.168.$netns.2" --ledger http://192.168.1.2:8000 ${@:2}
