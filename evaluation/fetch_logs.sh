49a303c3f084b9f6b6ecb7898d3bb68161cac666
Anton Curanz <anton@curanz.de>
2024-10-26 18:32:14 +0200

    deleted wallets and added evaluation scripts

---
+++ evaluation/fetch_logs.sh
@@ -0,0 +1,11 @@
+if [ "$#" -lt 2 ]; then
+    echo "Usage: $0 <name> <containers...>"
+    exit 1
+fi
+
+mkdir -p $1
+
+for container in "${@:2}"; do
+    log_file=$(ssh ant0n@192.168.108.4 "sudo podman exec $container ls logs")
+    ssh ant0n@192.168.108.4 "sudo podman exec $container cat logs/$log_file" > $1/$log_file
+done
